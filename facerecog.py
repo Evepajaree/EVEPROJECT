df22f5ff45714942db1f94a73c13709432a04e4f
Evepajaree <eve_102@hotmail.co.th>
2021-04-18 19:57:17 +0700

    tree

--- facerecog.py
+++ facerecog.py
@@ -31,7 +31,7 @@ def draw_boundary(img,classifier,scaleFactor,minNeighbors,color,clf):
       coords=[]
       for(x,y,w,h) in features:
             cv2.rectangle(img,(x,y),(x+w,y+h),color,2)
-            ,con = clf.predict(gray[y:y+h,x:x+w])
+            id,con = clf.predict(gray[y:y+h,x:x+w])
             
             
             if id ==1 :
